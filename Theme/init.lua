c76f51e7ff609dbba5631ea29bcbe14767111c18
SSHRK <cvetkovd975@yandex.ru>
2025-06-09 17:39:01 +0300

    Create init.lua

---
+++ Theme/init.lua
@@ -0,0 +1,20 @@
+UITheme = {
+    Dark = {
+        Background = Color3.fromRGB(20, 20, 20),
+        SideBar = Color3.fromRGB(30, 30, 30),
+        Text = Color3.fromRGB(255, 255, 255),
+        ElementColor = Color3.fromRGB(42, 42, 42),
+        Outline = Color3.fromRGB(62, 62, 62),
+        Placeholder = Color3.fromRGB(20, 20, 20),
+        IconColor = Color3.fromRGB(255, 255, 255),
+    },
+    Light = {
+        Background = Color3.fromRGB(180, 180, 180),
+        SideBar = Color3.fromRGB(215, 215, 215),
+        Text = Color3.fromRGB(30, 30, 30),
+        ElementColor = Color3.fromRGB(215, 215, 215),
+        Outline = Color3.fromRGB(230, 230, 230),
+        Placeholder = Color3.fromRGB(170, 170, 170),
+        IconColor = Color3.fromRGB(30, 30, 30),
+    },
+}
