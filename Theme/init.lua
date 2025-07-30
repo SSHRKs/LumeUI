7f75c4c2409c02f4d3426d8919196bd0c3e362c0
SSHRK <cvetkovd975@yandex.ru>
2025-07-30 16:09:33 +0300

    Update init.lua

--- Theme/init.lua
+++ Theme/init.lua
@@ -1,7 +1,7 @@
 UITheme = {
     Dark = {
         Background = Color3.fromRGB(20, 20, 20),
-        SideBar = Color3.fromRGB(30, 30, 30),
+        SideBar = Color3.fromRGB(28, 28, 28),
         Text = Color3.fromRGB(255, 255, 255),
         ElementColor = Color3.fromRGB(42, 42, 42),
         Outline = Color3.fromRGB(62, 62, 62),
@@ -16,5 +16,14 @@ UITheme = {
         Outline = Color3.fromRGB(230, 230, 230),
         Placeholder = Color3.fromRGB(170, 170, 170),
         IconColor = Color3.fromRGB(30, 30, 30),
+    },
+    Amethyst = {
+        Background = Color3.fromRGB(42, 16, 68),
+        SideBar = Color3.fromRGB(50, 24, 76),
+        Text = Color3.fromRGB(255, 255, 255),
+        ElementColor = Color3.fromRGB(36, 10, 62),
+        Outline = Color3.fromRGB(80, 22, 138),
+        Placeholder = Color3.fromRGB(58, 28, 88),
+        IconColor = Color3.fromRGB(255, 255, 255),
     },
 }
